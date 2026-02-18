319a0fe8e08bc008774107c92206b026a72dba48
Luboš Hrnčíř <luboshrncir07@gmail.com>
2026-02-18 14:38:44 +0100

    zmena

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-from flask import jsom
-from flask import os
+import json
+import os
 
 app = Flask(__name__)
 
@@ -21,6 +21,20 @@ def submit():
     package_weight = request.form.get('package_weight')
     package_dimensions = request.form.get('package_dimensions')
     delivery_type = request.form.get('delivery_type') 
+
+    if sender_name.strip() == "":
+        return "Jméno odesílatele nesmí být prázdné.", 400
+
+        #validace psč odesílatele "123 45"
+    if len(sender_postal_code) != 6:
+        return "PSČ odesílatele musí být ve formátu 123 45.", 400
+    providence, postal_code = sender_postal_code.split("")
+    if not (providence.isdigit() and postal_code.isdigit()):
+        return "PSČ odesílatele musí být ve formátu 123 45.", 400
+    if type not in ["balík", "dopis", "cenný balík"]:
+        return "Neplatný typ doručení.", 400
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
